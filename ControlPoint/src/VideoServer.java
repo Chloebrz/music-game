51338bfb4876662068a5f9217a758615ad3fcd24
jianfeipan <jianfei.pan.tb@gmail.com>
2016-03-01 11:07:35 +0100

    Control point back

---
+++ ControlPoint/src/VideoServer.java
@@ -0,0 +1,47 @@
+package src;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URI;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class VideoServer implements HttpHandler {
+
+	private MyControlPoint control;
+
+	public VideoServer(MyControlPoint control) {
+		this.control = control;
+	}
+
+	@Override
+	public void handle(HttpExchange t) throws IOException {
+
+		// -> /video -> requestBody:(html content)
+		URI uri = t.getRequestURI();
+		System.out.println(uri.toString());
+
+		int i;
+		StringBuilder sb = new StringBuilder();
+		InputStream in = t.getRequestBody();
+		InputStreamReader reader = new InputStreamReader(in);
+		while ((i = reader.read()) != -1) {
+			sb.append((char) i);
+		}
+
+		in.close();
+		String htmlbody = sb.toString();
+
+		String response = "This is the response";
+		t.getResponseHeaders().add("Access-Control-Allow-Origin", "http://localhost:8080");
+		t.sendResponseHeaders(200, response.length());
+		OutputStream os = t.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
+
+		control.displayHtml(htmlbody);
+	}
+}
